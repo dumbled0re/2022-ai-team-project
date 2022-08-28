865207b070fb4ac176e83b2663145f8e24e37265
dumbled0re <s.r19990706@gmail.com>
2022-08-29 00:44:02 +0900

    update: slack_notify.py

--- spider/utils/slack_notify.py
+++ spider/utils/slack_notify.py
@@ -5,16 +5,20 @@ import slackweb
 
 class SlackNotify:
     """
-    スラック通知
+    スラック通知のクラス
     """
 
     def __init__(self) -> None:
         self.slack_hook_url = os.getenv("SLACK_WEBHOOK_URL")
-        assert self.slack_hook_url is not None, "環境変数にSLACK_WEBHOOK_URLが設定されていません。"
+        assert (
+            self.slack_hook_url is not None
+        ), "環境変数にSLACK_WEBHOOK_URLが設定されていません。"
 
     def slack_notify(self, text: str) -> None:
-        """
-        スラック通知
+        """スラックに通知を送るメソッド
+
+        Args:
+            text (str): 送る文字列
         """
         slack = slackweb.Slack(url=self.slack_hook_url)
         slack.notify(text=text)
